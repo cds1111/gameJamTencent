a10db66d9285f3c486167461ebab24ad20eef1d0
jmjp <13476168763@163.com>
2026-04-18 18:37:35 +0800

    关卡标题

--- scripts/level/LevelController.gd
+++ scripts/level/LevelController.gd
@@ -4,22 +4,30 @@ class_name LevelController
 @export var player_scene: PackedScene = preload("res://scenes/entities/Player.tscn")
 @export var starting_ability: Resource
 @export var next_level_id: int = 2
+@export var level_title_text: String = "LEVEL"
 
 @onready var _entities: Node2D = $"../Entities"
 @onready var _player_spawn: Marker2D = $"../Entities/PlayerSpawn"
 @onready var _goal: Area2D = $"../Entities/Goal"
+@onready var _title_label: Label = $"../LevelUI/TopRightMargin/TitleBackground/TitleText"
 
 var _player: CharacterBody2D
 var _is_restarting: bool = false
 
 
 func _ready() -> void:
+	_apply_level_title()
 	_spawn_player()
 	if is_instance_valid(_goal):
 		_goal.body_entered.connect(_on_goal_body_entered)
 	var signal_manager := get_node_or_null("/root/SignalManager")
 	if signal_manager != null and not signal_manager.player_died.is_connected(_on_player_died):
 		signal_manager.player_died.connect(_on_player_died)
+
+
+func _apply_level_title() -> void:
+	if is_instance_valid(_title_label):
+		_title_label.text = level_title_text
 
 
 func _process(_delta: float) -> void:
